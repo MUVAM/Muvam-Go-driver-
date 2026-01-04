423e9238562b42532f64ac46f878d5e1d13cd139
Joseph <josephchukwuebuka10@gmail.com>
2026-01-04 13:57:37 +0000

    toadd

---
+++ lib/core/config/firebase_service_account.dart
@@ -0,0 +1,24 @@
+// ⚠️ WARNING: This file contains sensitive credentials
+// DO NOT commit this file to version control
+// This file should be in .gitignore
+
+class FirebaseServiceAccount {
+  // TODO: Replace these values with your actual Firebase service account credentials
+  // You can get these from Firebase Console > Project Settings > Service Accounts > Generate New Private Key
+
+  static const Map<String, dynamic> credentials = {
+  "type": "service_account",
+    "project_id": "muvam-go",
+    "private_key_id": "5a7842d9629c3eeadf073440f840897d5bb51179",
+    "private_key":
+        "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCPGp2ECwkl7Hf/\nOrLB6rGHkNhevLJr2WoYMPKOKBzu0Hd7T+iyplg2t3zRT2WD+ryFH3fP4h9PPcxY\nV0A0aKAwt1W9TMoA4oewdH6o15EqaadNBcyM/hix5P9p1XTQ90Yr+kR0Dbo5PTHH\ngHt+Ic3RgGBFxqoo0emSEZmOO8xzsIEIiqN1xCTbFqBnaXicj2O6lWdVuhEwZR7r\nh16GoE+QNKSpA7KZyRepHx5P4lrjWB7s3NME5Cv9QYYzf6p0GfducM4sL7D0Rqds\n5Wy5vtuUQUYGbaC9p8so0ckXYiMbmLc472MaRYofpHNhF8oTct0Nq0YGXmNwON6B\ns6hR+e6bAgMBAAECggEAIBzOXQum7WIvmVAgPgC+/tdjnnrEpQwAUVZmj7zzpSmV\nrk/DvZczc2/gbA3fj2rMNClYFwe8k+kfunE14KzbyaLSdCzPpbwvy8qQg64ADt7v\nooqbGMaKXx0urZ6vXJcooYTDKLHN6LtRON4dmDPYvlsK/41xsMBOJC+b4D2OByWu\nAvtujX7Uv11Pmmubpz+12ed3IH1y8Yv+G8pG9/9KEP/NvY4bkdqy/EY8HUiiL6ZO\nCHrwR2rvDxgSuC7JXSspPl3LNNBRK8lYsEd5+qq/VdXnmWjsLgEVo8+BeBNyBwTX\nokUq+FMJMVMm9uum1zwjF5zw0HgPWlqBfWsqnXVHwQKBgQDJegcFfVBxahL/22po\nOGPiQUDXuRbD3P3us7oslg1N9vPRFbTdrfwEPVBrycVNs73cy5cSMD4hh3nveJDJ\n39iAIg/sxewIVZlLs6zmnVtc8zZ2i64fu7NbQfpDvsyDxgSwnR4KCnLYRaIjxlsD\nOV9mM4nUu+zPQaNlcH1+9B8dWwKBgQC11J91mrUGtKqf1EJKJHP4cSJ6z5rlP3Ak\nRYvsCTxgu6k0ebaTreekY6pp8aa54yHx3mlhpDoAx/l2v6RE1kouDE9yIV2skclr\nU7LgBFYpH4Tuqvc/yKvu7t9vstwsGUpYzcezOQ+M3RNN2o8jmYn52JtfflHL190C\nxMy1MDb3wQKBgQCj1exRxXad04FBt/5OY9StjxyYj7V4boO8EUDKsvZ8uN3J35nl\nTm8KT4XPMROVvR0eqBvgOYS8pTs9xdAPqUyX3ZsPkl7+5AS1b5WhLsK10j0upyVg\nPrV59yv43PXYDjWQHSPpTFfZkL5p0wn9lDEEndJuQKcwXpLtkU0r6H5I0wKBgQCf\n0WHS2YcRfMyXrNYTGtU/4hi/PyhTJR4pE2O3tCp4j5lagTxjsy/uApfvhLHZovTc\nBXQ0mUdOFnaC1FDLHFqY/ML7eq4+Fumlxyxq0AT3IjgSGv009+l/GkATim8Q5628\nPiqvsZCkbFAiVt8HGFX431U5aur0MyiT3WxcOSOnQQKBgE5Y0gIR/oFybCaC3I7J\n1C7bPEaQrOhOAIqG6G0em7qrMeeLQeZnoaWpjjqe2JcGHRxe8k1R22H2hts5Vukf\nUoQOWkKxw7xiUh9EvrE5yU8OEY8vw2xjZrQ7d5hrck6X1VF1Kn6mqFavRCzoZZa5\nVGUwkOlp6x8/l82sUhIN8bWO\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-fbsvc@muvam-go.iam.gserviceaccount.com",
+    "client_id": "103593346923649190493",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url":
+        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40muvam-go.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };
+}
