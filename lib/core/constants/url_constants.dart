abab0c68e9f17647cdc3e223edc69e9c7c59e1e1
Favour Williams <hello.williamsfav@gmail.com>
2026-01-28 10:53:36 +0100

    fixed: Update baseurl in constants file

--- lib/core/constants/url_constants.dart
+++ lib/core/constants/url_constants.dart
@@ -12,8 +12,8 @@ class UrlConstants {
   static const String resendOtp = "/otp/resend";
   static const String verifyOtp = "/otp/verify";
   static const String registerUser = "/users/register_driver";
-  static const String completeProfile = "/api/v1/users/profile/complete";
-  static const String favouriteLocation = "/api/v1/users/favouriteLocation";
+  static const String completeProfile = "/users/profile/complete";
+  static const String favouriteLocation = "/users/favouriteLocation";
   static const String updateLocation = "/users/location";
   static const String rideEstimate = "/rides/estimate";
   static const String userProfile = "/users/profile";
